736d89e1deab97117252757ea58fad501a4c3ec9
thebestjuliana <76972376+thebestjuliana@users.noreply.github.com>
2021-01-23 12:24:42 -0500

    updated code to reflect original case entries

--- CapstoneWeek1_PigLatin/Program.cs
+++ CapstoneWeek1_PigLatin/Program.cs
@@ -16,29 +16,58 @@ namespace CapstoneWeek1_PigLatin
                 Console.WriteLine("Enter a line to be translated: ");
 
                 //store input as a variable
-
-                string input = Console.ReadLine().ToLower();
+                string ogInput = Console.ReadLine();
+                string input = ogInput.ToLower();
 
                 //if input is null or whitespace prompt user to try again and go to top of loop
 
-                if(string.IsNullOrWhiteSpace(input))
+                if (string.IsNullOrWhiteSpace(input))
                 {
-                    Console.WriteLine("invalid data, please try again");
+                    Console.WriteLine("Invalid data. Please enter something to be translated.");
                     continue;
                 }
 
                 //translate into piglatin and display on consul
-                foreach(string word in StringToArray(input)) 
+                string[] arrayOfWords = StringToArray(input);
+                string[] lowerTranslatedPigLatin = new string[arrayOfWords.Length];
+                for (int i = 0; i < arrayOfWords.Length; i++)
                 {
-                    //don't translate extra spaces Ex: 2 spaces between words (remove the extra space)
-                    if(word == "" || word == null)
+
+                    if (string.IsNullOrEmpty(arrayOfWords[i]))
                     {
+                        lowerTranslatedPigLatin[i] = " ";
+
                         continue;
                     }
-                    Console.Write(TranslatePigLatin(word) + " ");
+
+
+
+                    lowerTranslatedPigLatin[i] = TranslatePigLatin(arrayOfWords[i]);
+
                 }
+
+                // Let't try to translate the piglatin back into the Original case format
+                string[] ogWords = ogInput.Split();
+
+
+                int numberOfWordsInString = ogWords.Length;
+
+                //Let's look at each letter of the ogWords
+                string[] correctCaseTranslation;
+                correctCaseTranslation = new string[numberOfWordsInString];
+
+                for (int i = 0; i < numberOfWordsInString; i++)
+                {
+
+                    correctCaseTranslation[i] = FixCase(ogWords[i], lowerTranslatedPigLatin[i]);
+                }
+                string finalResult = String.Join(" ", correctCaseTranslation);
+
+                Console.WriteLine(finalResult);
+
+
                 //Application asks user if they want to translate another word
-                
+
 
                 translatorRunning = YesNoValidation();
 
@@ -46,52 +75,61 @@ namespace CapstoneWeek1_PigLatin
             }
         }
 
-        public static string TranslatePigLatin(string word)
+        public static string TranslatePigLatin(string arrayOfWords)
         {
             //does the word contain symbol? if yes- don't translate
-            
-            if (ContainsSymbol(word) == true)
+
+            if (ContainsSymbol(arrayOfWords) == true)
             {
-                return word;
-            } 
+                return arrayOfWords;
+            }
             //does the word contain a number? if yes - don't translate
-            if (ContainsNumber(word) == true)
+            if (ContainsNumber(arrayOfWords) == true)
             {
-                return word;
+                return arrayOfWords;
             }
 
             //figure out the first letter of the string
-            
-            string firstletter = word.Substring(0, 1);
+
+            string firstletter = arrayOfWords.Substring(0, 1);
 
             //figure out the last letter of string if ends with punctuation
-            char lastletter = word.ToCharArray()[word.Length - 1];
-            
-            
+            char lastletter = arrayOfWords.ToCharArray()[arrayOfWords.Length - 1];
+
+
             if (char.IsPunctuation(lastletter) == true)
             {
                 string translatedWord;
-                translatedWord = TranslateWithPunctuation(word);
+                translatedWord = TranslateWithPunctuation(arrayOfWords);
                 return translatedWord;
             }
+            int indexOfY = arrayOfWords.IndexOf('y');
 
             //if starts with vowel
+
             if (firstletter == "a" || firstletter == "e" || firstletter == "i" || firstletter == "o" || firstletter == "u")
             {
 
-                return word + "way";
+                return arrayOfWords + "way";
             }
-            else if (IsIndexOfVowel(word) != -1)
+            else if (IsIndexOfVowel(arrayOfWords) != -1)
             {
-                string wordStartingWithFirstVowel = word.Substring(IsIndexOfVowel(word));
-                string wordBeforeFirstVowel = word.Substring(0, IsIndexOfVowel(word));
+                string wordStartingWithFirstVowel = arrayOfWords.Substring(IsIndexOfVowel(arrayOfWords));
+                string wordBeforeFirstVowel = arrayOfWords.Substring(0, IsIndexOfVowel(arrayOfWords));
 
                 return wordStartingWithFirstVowel + wordBeforeFirstVowel + "ay";
 
             }
+            else if (indexOfY != -1)
+            {
+                string wordStartingWithFirstY = arrayOfWords.Substring(arrayOfWords.IndexOf('y'));
+                string wordBeforeY = arrayOfWords.Substring(0, arrayOfWords.IndexOf('y'));
+
+                return wordStartingWithFirstY + wordBeforeY + "ay";
+            }
             else
             {
-                return word + "way";
+                return arrayOfWords + "way";
 
             }
 
@@ -123,7 +161,7 @@ namespace CapstoneWeek1_PigLatin
                 else
                 {
                     Console.Beep();
-                    Console.WriteLine("invalid answer");
+                    Console.WriteLine("Invalid answer. Play again? y/n?");
                 }
             }
             return false;
@@ -143,6 +181,10 @@ namespace CapstoneWeek1_PigLatin
             int count = 0;
             foreach (char letter in lettersInWord)
             {
+                if (letter.Equals('@'))
+                {
+                    count++;
+                }
 
 
                 if (Char.IsSymbol(letter))
@@ -184,8 +226,8 @@ namespace CapstoneWeek1_PigLatin
         {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 
-           return word.IndexOfAny(vowels);
-            
+            return word.IndexOfAny(vowels);
+
         }
 
         public static string TranslateWithPunctuation(string word)
@@ -194,27 +236,71 @@ namespace CapstoneWeek1_PigLatin
 
             //if starts with vowel
 
+            int indexOfy = word.IndexOf('y');
+
             string lastLetter = word.Substring(word.Length - 1);
             string wordWithoutLastLetter = word.Substring(0, word.Length - 2);
 
             if (firstletter == "a" || firstletter == "e" || firstletter == "i" || firstletter == "o" || firstletter == "u")
             {
-                
-                
+
+
 
                 return wordWithoutLastLetter + "way" + lastLetter;
             }
             else if (IsIndexOfVowel(word) != -1)
             {
-                string wordStartingWithFirstVowel = word.Substring(IsIndexOfVowel(word), word.Length-2);
+                string wordStartingWithFirstVowel = word.Substring(IsIndexOfVowel(word), word.Length - 2);
                 string wordBeforeFirstVowel = word.Substring(0, IsIndexOfVowel(word));
 
                 return wordStartingWithFirstVowel + wordBeforeFirstVowel + "ay" + lastLetter;
 
             }
+            else if (indexOfy != -1)
+            {
+                string wordStartingWithFirstY = word.Substring(word.IndexOf('y'));
+                string wordBeforeY = word.Substring(0, word.IndexOf('y'));
+
+                return wordStartingWithFirstY + wordBeforeY + "ay";
+            }
             else
             {
+
                 return wordWithoutLastLetter + "way" + lastLetter;
+
+            }
+        }
+
+        public static string FixCase(string ogWords, string lowerTranslatedPigLatin)
+        {
+            char[] ogWord = ogWords.ToCharArray();
+            char[] lowerTranslatedPigWord = lowerTranslatedPigLatin.ToCharArray();
+            char[] fixedCase = new char[lowerTranslatedPigLatin.Length];
+
+            {
+                for (int e = 0; e < ogWord.Length; e++)
+                {
+                    if (Char.IsUpper(ogWord[e]) == true)
+                    {
+                        fixedCase[e] = Char.ToUpper(lowerTranslatedPigWord[e]);
+
+                    }
+                    else
+                    {
+
+                        fixedCase[e] = lowerTranslatedPigWord[e];
+
+                    }
+                }
+
+                for (int f = ogWord.Length; f < lowerTranslatedPigWord.Length; f++)
+                {
+                    fixedCase[f] = lowerTranslatedPigWord[f];
+                }
+                string fixedCaseTranslation = new string(fixedCase);
+
+                string sendToUser = string.Join("", fixedCaseTranslation);
+                return sendToUser;
 
             }
         }
